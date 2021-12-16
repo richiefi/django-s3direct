10bbe75d25c30a11ccb05ed47cbea913c05408b8
jsayles <jsayles@gmail.com>
2021-12-16 13:54:04 -0800

    updated url file to post django 3.0 path method

--- s3direct/urls.py
+++ s3direct/urls.py
@@ -1,9 +1,8 @@
-from django.urls import re_path
+from django.urls import path
+
 from s3direct.views import get_upload_params, generate_aws_v4_signature
 
 urlpatterns = [
-    re_path(r'^get_upload_params/', get_upload_params, name='s3direct'),
-    re_path(r'^get_aws_v4_signature/',
-            generate_aws_v4_signature,
-            name='s3direct-signing'),
+    path("^get_upload_params/", get_upload_params, name="s3direct"),
+    path("^get_aws_v4_signature/", generate_aws_v4_signature, name="s3direct-signing"),
 ]
